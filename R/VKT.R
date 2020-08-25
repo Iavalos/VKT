fbe19dd8e3e20e7f086d792b5f2fc3d25cc65573
Iavalos <i.avalosvargas@gmail.com>
2020-08-24 23:05:37 -0500

    Add the policy elasticities

--- R/VKT.R
+++ R/VKT.R
@@ -1,6 +1,13 @@
 VKT <- function(population,employment, distance_per_trip,
                 trips_per_person, trips_per_employee,
-                selected_mode_fraction, selected_purpose_fraction){
+                selected_mode_fraction, selected_purpose_fraction,mode,policy_elasticity=NA,
+                Npt = NA){
+
+  if(is.na(policy_elasticity)){
+    selected_mode_fraction = selected_mode_fraction
+  } else { if(mode == "Car"){selected_mode_fraction = selected_mode_fraction*(1-(policy_elasticity/100))}else
+  {if(mode != "Car"){selected_mode_fraction = selected_mode_fraction*(1+(abs(policy_elasticity/100)/Npt))}}
+  }
 
   trips_all_modes <-
     (trips_per_person * population) +
--- R/VKT_table.R
+++ R/VKT_table.R
@@ -1,4 +1,4 @@
-VKT_table <- function(unique.ID, location.ID, df.base, df.assumptions, mode, purpose){
+VKT_table <- function(unique.ID, location.ID, df.base, df.assumptions, mode, purpose,policy=NA,Npt=NA){
   pop = subset(df.assumptions, Category == "Trip_rates"& Subcategory=="Pop" & Location.ID == location.ID)$Value *
     subset(df.base, MMU.ID==unique.ID & location.ID == location.ID)$population
   job = subset(df.assumptions, Category == "Trip_rates"& Subcategory=="Jobs"& Location.ID == location.ID)$Value *
@@ -13,7 +13,15 @@ VKT_table <- function(unique.ID, location.ID, df.base, df.assumptions, mode, pur
   distance = subset(df.assumptions, Category == "Travel_distance"& Subcategory==purpose &
                       Location.ID==location.ID)$Value
   mode.trips =  p.t * vehicle
-  split = subset(df.assumptions, Category == "Trip_split"& Subcategory==purpose)$Value
+
+  if(is.na(policy)){
+    split = split
+  } else { if(mode == "Car"){split = split*(1-(subset(df.assumptions,
+                                                      Category == "Scenarios"& Subcategory==policy)$Value)/100)}
+    else{split= split*(1+(abs((subset(df.assumptions,
+                                      Category == "Scenarios"& Subcategory==policy)$Value)/100)/Npt))}
+  }
+
   VKT = mode.trips *distance*split
   return(VKT)
 }
